517ba7e02170ba811dbaf8090edfd78fb7c8e817
anil gupta <anil6080@gmail.com>
2016-02-22 17:26:18 +0530

    initial commit

---
+++ lib/Akamai/Exception/EdgeAuthParameterException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Akamai\Exception;
+
+class EdgeAuthParameterException extends \Exception {
+}
---
+++ lib/Akamai/Exception/EdgeAuthParameterExceptionPD.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Akamai\Exception;
+
+class EdgeAuthParameterExceptionPD extends \Exception {
+}
